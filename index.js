7db5dd5a327df0c074cff719d525c339ef6c639a
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-03-15 21:37:23 -0500

    created variables for index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
+const createTeamProfileGenerator = require("./src/document.js");
 
-co
+const Engineer = require ('./lib/engineer.js');
+const Intern = require ('./lib/intern.js');
+const Manager = require ('./lib/manager.js');
+const employees = [];
+
+const validateInput = (userInput)=> {
+  if(userInput==="") {
+    return "Cannot leave blank. Please type your response before proceeding.";
+  }else{
+    return true;
+  }
+};
+
+// Array of questions for terminal/CLI
