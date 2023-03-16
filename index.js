f24a687c56c3dda3cd4b563b6db039b86410dbb0
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-03-16 17:09:08 -0500

    corrected errors in index.js

--- index.js
+++ index.js
@@ -1,22 +1,70 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const createTeamProfileGenerator = require("./src/document.js");
+const createTeamProfileGenerator = require("./src/genrateHTML.js");
 
-const Engineer = require ('./lib/engineer.js');
-const Intern = require ('./lib/intern.js');
-const Manager = require ('./lib/manager.js');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
+const Manager = require('./lib/manager.js');
 const employees = [];
 
-const validateInput = (userInput)=> {
-  if(userInput==="") {
+let isDirectoryComplete = false;
+
+const validateInput = (userInput) => {
+  if (userInput === "") {
     return "Cannot leave blank. Please type your response before proceeding.";
-  }else{
+  } else {
     return true;
   }
 };
 
-// Array of questions for terminal/CLI
+const innit = async () => {
+  await createManager();
+
+  while (!isDirectoryComplete) {
+    const employeeQuestions = [
+      {
+        type: 'list',
+        message: "Select employee type you wish to add:",
+        name: "employeeType",
+        choices: [
+          { name: 'Engineer' },
+          { name: 'Intern' },
+          { name: 'None' },
+        ],
+      },
+    ];
+
+    const { employeeTeamMember } = await inquirer.prompt(employeeQuestions);
+
+    if (employeeTeamMember === 'none') {
+      isDirectoryComplete = true;
+    } else {
+      if (employeeTeamMember === "engineer") {
+        await newEngineer();
+      }
+      if (employeeTeamMember === "intern") {
+        await newIntern();
+      }
+    }
+
+  }
+
+  const completeDirectory = () => {
+    console.log('Directory completed');
+    fs.writeFileSync('index.html', completeDirectory, HTML, createTeamProfileGenerator, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("HTML file created");
+      }
+    })
+  }
+
+
+}
+
+// Array of questions for terminal / CLI
 const questionsManager = () => {
   return inquirer.prompt([
     {
@@ -47,36 +95,11 @@ const questionsManager = () => {
     const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber)
     employees.push(manager);
     promptEmployeeMenu()
+
+    questionsManager();
   })
 }
 
-const employeeQuestions=[
-  {
-    type: 'list',
-    message: "Select employee type you wish to add:",
-    name: "employeeType",
-    choices: [
-      {name: 'Engineer'},
-      {name: 'Intern'},
-      {name: 'None'},
-    ],
-  },
-];
-
-const{employeeType} = () => {
-  return inquirer.prompt(employeeQuestions);
-}
-
-if(employeeType === "none"){
-  isTeamComplete = true;
-} else {
-  if (employeeType === 'engineer'){
-    return newEngineer();
-  }
-  if (employeeType === 'intern'){
-    return newIntern();
-  }
-}
 
 const newEngineer = async () => {
   const engineerQuestions = [
@@ -111,51 +134,48 @@ const newEngineer = async () => {
   const engineer = new Engineer(engineerAnswers);
 
   employees.push(engineer);
-
-  // Create Intern
-
-  const newIntern = async () => {
-    const internQuestions = [
-      {
-        type: 'input',
-        message: "Enter Intern name:",
-        name: 'name',
-        validate: validateInput,
-      },
-      {
-        type: 'input',
-        message: "Enter Intern ID number:",
-        name: 'id',
-        validate: validateInput,
-      },
-      {
-        type: 'input',
-        message: 'Enter Intern email:',
-        name: 'email',
-        validate: validateInput,
-      },
-      {
-        type: 'input',
-        message: 'Enter Intern school name:',
-        name: 'school',
-        validate: validateInput,
-      },
-    ];
-    // Answers input for intern will be generated from user input
-    const internAnswers = await inquirer.prompt(internQuestions);
-
-    const intern = new Intern(internAnswers);
-
-    employeeQuestions.push (intern);
-  }
 }
+
+// Create Intern
+const newIntern = async () => {
+  const internQuestions = [
+    {
+      type: 'input',
+      message: "Enter Intern name:",
+      name: 'name',
+      validate: validateInput,
+    },
+    {
+      type: 'input',
+      message: "Enter Intern ID number:",
+      name: 'id',
+      validate: validateInput,
+    },
+    {
+      type: 'input',
+      message: 'Enter Intern email:',
+      name: 'email',
+      validate: validateInput,
+    },
+    {
+      type: 'input',
+      message: 'Enter Intern school name:',
+      name: 'school',
+      validate: validateInput,
+    },
+  ];
+  // Answers input for intern will be generated from user input
+  const internAnswers = await inquirer.prompt(internQuestions);
+
+  const intern = new Intern(internAnswers);
+
+  employeeQuestions.push(intern);
+}
+
 
 innit();
 
-const HTML = generateHTML(employees);
-  fs.writeFileSync('teams.html', createTeamProfileGenerator, HTML, 'utf-8')
-  console.log("HTML file has been created");
 
 
-questionsManager();
+
 
