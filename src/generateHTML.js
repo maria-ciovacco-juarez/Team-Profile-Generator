deae3abcb711f1f953973bf6be1e83bc4570837b
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-16 14:18:47 -0500

    added templates

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,7 +2,7 @@ const generateHTML = team => {
   const html = [];
   //manager html section
   const createManager = manager => {
-    let managerHtml= ` 
+    let managerHtml = ` 
     <div class="column">
         <div class="card">
             <div class="container">
@@ -15,7 +15,47 @@ const generateHTML = team => {
         </div>
         </div>
           `;
-        html.push(managerHtml);
+    html.push(managerHtml);
   };
+
+  //engineer html section
+  const createEngineer = engineer => {
+    let engineerHtml = ` 
+    <div class="column">
+        <div class="card">
+            <div class="container">
+                <h2>${engineer.getName()}</h2>
+                <h3>Title: ${engineer.getRole()}</h3>
+                <p>Employee ID: ${engineer.getId()}</p>
+                <p>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+                <p>GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></p>  
+            </div>
+        </div>
+        </div>
+          `;
+    html.push(engineerHtml);
+  };
+
+  const createIntern = intern => {
+    let internHtml = ` 
+    <div class="column">
+        <div class="card">
+            <div class="container">
+                <h2>${intern.getName()}</h2>
+                <h3>Title: ${intern.getRole()}</h3>
+                <p>Employee ID: ${intern.getId()}</p>
+                <p>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+                <p>School: ${intern.getSchool()}</p>    
+            </div>
+        </div>
+        </div>
+          `;
+          html.push(internHtml)
+  };
+
+  
+
+
+
 }
 
