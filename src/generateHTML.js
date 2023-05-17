679e3f98fbb461e886e8633def77e30ce0ded64b
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-16 23:28:06 -0500

    made corrections to files

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -78,7 +78,7 @@ module.exports = team => {
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>The Folklore Bunch</title>
-      <link rel="stylesheet" href="./dist/style.css"/>
+      <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
       <h1>The Folklore Bunch</h1>
